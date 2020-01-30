b3fbf0bad9608f361f77222fa7da470b32c693c4
muhammadawaisshaikh <awaisshaikh94@gmail.com>
2020-01-30 16:24:23 +0500

    ruby_lambda

---
+++ ruby_lambda.rb
@@ -0,0 +1,14 @@
+# Write a lambda which takes an integer and square it
+square      = ->(a){a**2 }
+
+# Write a lambda which takes an integer and increment it by 1
+plus_one    = ->(b){b+1} 
+
+# Write a lambda which takes an integer and multiply it by 2
+into_2      = -> (c){c*2} 
+
+# Write a lambda which takes two integers and adds them
+adder       = ->(d,e){d+e} 
+
+# Write a lambda which takes a hash and returns an array of hash values
+values_only = ->(f){f.values}
