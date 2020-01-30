7058fb022fbe297aa9ca35a973e0729a78053fa6
muhammadawaisshaikh <awaisshaikh94@gmail.com>
2020-01-30 12:10:26 +0500

    group_by

---
+++ ruby_group_by.rb
@@ -0,0 +1,9 @@
+marks = {"Ramesh":23, "Vivek":40, "Harsh":88, "Mohammad":60}
+
+def group_by_marks(marks, pass_marks)
+    marks.group_by { |key, value|
+        value<pass_marks ? "Failed" : "Passed"
+    }
+end
+
+puts group_by_marks(marks, 30);
